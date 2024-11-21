727b6d3198719e217fcb07d150a872046b7bb838
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-21 23:14:50 +1030

    setting stabel AOS animtion

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+
+import Aos from "aos";
 
 const Navbar = () => {
+
+    useEffect(() => {
+        Aos.init({
+          duration: 500,
+          easing: "ease-in-out",
+          mirror: true,
+        }); // Initialize AOS with desired options
+      }, []);
+    
   return (
-    <div className='text-3xl text-white'>Navbar</div>
+    <div         data-aos="fade-up"
+     className='text-3xl text-white'>Navbar</div>
   )
 }
 
