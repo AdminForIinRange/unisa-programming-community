fe5a110a01edf8e0c25b9921eff5cfe079b5a8ef
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-21 18:28:48 +1030

    commit 2 lol

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,27 @@
-import React from "react";
-// This export statement forces the layout to be dynamic, meaning it will not be statically optimized by Next.js
+// Async/Await Requirement:
 
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+// When using await, the function containing it must be declared as async. Otherwise, JavaScript will throw a syntax error.
+// Issue of Missing async:
+
+// If you forget to declare a function as async but use await inside it, the function will not behave as expected. It may lead to runtime errors or unexpected behavior, like the function skipping the try block and going directly to the catch block.
+// Catch Without Try:
+
+// In your case, the issue seems to be that the lack of async caused the promise rejection to bypass normal flow, leading to confusion about why the function immediately hit the catch block.
+
+import React from "react";
+
+
+
+import "../globals.css";
+import { redirect } from "next/navigation";
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+ 
+
+  return (
+   
+        <div>{children}</div>
+     
+  );
 };
 
-export default layout;
+export default Layout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const Home = () => {
+  return <div>hi</div>;
+};
 
-export default page
+export default Home;
