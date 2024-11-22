e2362a5b625164ba6fbc515f7c874f81570b829c
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-22 22:31:06 +1030

    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+/* eslint-disable tailwindcss/no-unnecessary-arbitrary-value */
 "use client";
 import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css"; // Import AOS CSS here if it's not imported globally
+import Link from "next/link";
 
 const Navbar = () => {
   useEffect(() => {
@@ -13,18 +16,60 @@ const Navbar = () => {
   }, []);
 
   //  AOS (Animate On Scroll) works by detecting when elements come into view, and for it to trigger, there must be enough content (or height) in the page for scrolling to occur. AOS relies on the element's position relative to the viewport (its Y value) to decide when to animate.
+  const links = [
+    {
+      name: "About Us",
+      link: "#aboutUs",
+      onClick: null,
+    },
+    { name: "Events  ", link: "#events", onClick: null },
+    { name: "Open Source", link: "#openSource", onClick: null },
+
+    { name: "sponsors", link: "#sponsors", onClick: null },
+    {
+      name: "Contact",
+      link: "",
+
+      speicals: true,
+    },
+    {
+      name: "Join Us",
+      link: "",
+
+      speicals: true,
+    },
+    {
+      name: "Login",
+      link: "",
+
+      speicals: true,
+    },
+  ];
 
   return (
     <>
-      <div className="fixed  w-100 h-100 bg-red sm:bg-green md:bg-blue lg:bg-yellow-500 xl:bg-purple-500">
-    BreakPoint
+      <div className="w-100  h-100 fixed bg-red sm:bg-green md:bg-blue lg:bg-yellow-500 xl:bg-purple-500">
+        BreakPoint
       </div>
       <div
         // Animation for scrolling up
-        className="fixed  w-full  flex items-center justify-center mt-[10px] px-[10px] "
+        className="fixed  mt-[10px]  flex w-full items-center justify-center px-[10px] "
       >
-        <div className=" bg-[#18181C] w-[100%] md:w-[70%]  lg:w-[50%] h-[65px] border-[#666666] border-[1.5px] rounded-xl hover:h-[75px] hover:md:w-[80%] hover:lg:w-[55%]   ease-in duration-300 hover:ease-out ease-in duration-300  ">
-       
+        <div className=" h-[65px] w-full rounded-xl  border-[1.5px] border-[#666666] bg-[#18181C] duration-300 ease-in hover:h-[75px]   hover:ease-out md:w-[70%] hover:md:w-4/5 lg:w-[50%] hover:lg:w-[55%]  ">
+          <div className="flex size-full items-center justify-center  ">
+            {links.map(({ name, link, speicals, onClick }, index) => (
+              <div
+                key={index}
+                className="flex h-full cursor-pointer items-center justify-center gap-[100px] text-nowrap   text-white duration-300  ease-in hover:ease-out "
+              >
+                <div className=" rounded-xl px-3 py-2 duration-300 ease-in hover:bg-black ">
+                  <Link href={link} />
+
+                  {name}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
