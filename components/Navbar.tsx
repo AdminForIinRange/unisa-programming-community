9a477010dde66dab9acf6b061a8b6f122d79f8f4
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-26 12:19:06 +1030

    stable

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,131 @@
 "use client";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "../components/ui/navbar-menu";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "../components/ui/navbar-menu";
 import { cn } from "@/lib/utils";
-
+import UPC_logo_Trans from "../public/png/UPC_logo_Trans.png";
+import Image from "next/image";
 
 export default function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
-    <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 ", className)}
-    >
-      <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Services">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Products">
-          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-       
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Pricing">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
-      </Menu>
-    </div>
+    <>
+      <div className="w-100  h-100 2xl:bg-orange-500 fixed bg-red sm:bg-green md:bg-blue lg:bg-yellow-500 xl:bg-purple-500">
+        BreakPoint
+      </div>
+      <div
+        className={cn(
+          "fixed   justify-center items-center top-5 inset-x-0 max-w-2xl mx-auto z-50   hidden md:flex transition ease-in-out delay-150 ",
+          className,
+        )}
+      >
+        <div className=" text-center ">
+          <Menu setActive={setActive}>
+            <div className="   rounded-xl   ">
+              <MenuItem setActive={setActive} active={active} Textcolor={false} item="About us">
+                <div className="flex flex-col space-y-4 text-sm">
+                  <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                  <HoveredLink href="/interface-design">
+                    Interface Design
+                  </HoveredLink>
+                  <HoveredLink href="/seo">
+                    Search Engine Optimization
+                  </HoveredLink>
+                  <HoveredLink href="/branding">Branding</HoveredLink>
+                </div>
+              </MenuItem>
+            </div>
+
+            <div className="   rounded-xl ">
+              <MenuItem setActive={setActive} active={active} Textcolor={false} item="Events">
+                <div className="flex flex-col space-y-4 text-sm">
+                  <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                  <HoveredLink href="/interface-design">
+                    Interface Design
+                  </HoveredLink>
+                  <HoveredLink href="/seo">
+                    Search Engine Optimization
+                  </HoveredLink>
+                  <HoveredLink href="/branding">Branding</HoveredLink>
+                </div>
+              </MenuItem>
+            </div>
+            <div className="   rounded-xl ">
+              <MenuItem Textcolor={false}
+                setActive={setActive}
+                active={active}
+                item="Open Source"
+              >
+                <div className="flex flex-col space-y-4 text-sm">
+                  <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                  <HoveredLink href="/interface-design">
+                    Interface Design
+                  </HoveredLink>
+                  <HoveredLink href="/seo">
+                    Search Engine Optimization
+                  </HoveredLink>
+                  <HoveredLink href="/branding">Branding</HoveredLink>
+                </div>
+              </MenuItem>
+            </div>
+
+            <div className="   rounded-xl ">
+              <MenuItem setActive={setActive} active={active} Textcolor={false} item="sponsors">
+                <div className="flex flex-col space-y-4 text-sm">
+                  <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                  <HoveredLink href="/interface-design">
+                    Interface Design
+                  </HoveredLink>
+                  <HoveredLink href="/seo">
+                    Search Engine Optimization
+                  </HoveredLink>
+                  <HoveredLink href="/branding">Branding</HoveredLink>
+                </div>
+              </MenuItem>
+            </div>
+            <div className="   rounded-xl ">
+              <MenuItem setActive={setActive} active={active} Textcolor={false} item="contact">
+                <div className="  grid grid-cols-2 gap-10 p-4 text-sm"></div>
+              </MenuItem>
+            </div>
+
+            <div className="   rounded-xl ">
+              {" "}
+              <MenuItem setActive={setActive} active={active} Textcolor={false} item="Join Us">
+                <div className="flex flex-col space-y-4 text-sm">
+                  <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                  <HoveredLink href="/interface-design">
+                    Interface Design
+                  </HoveredLink>
+                  <HoveredLink href="/seo">
+                    Search Engine Optimization
+                  </HoveredLink>
+                  <HoveredLink href="/branding">Branding</HoveredLink>
+                </div>
+              </MenuItem>{" "}
+            </div>
+
+            <div className="flex rounded-xl bg-white  ">
+              <MenuItem setActive={setActive} active={active} Textcolor={true} item="Login">
+                <div className="flex flex-col space-y-4 text-sm">
+                  <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                  <HoveredLink href="/interface-design">
+                    Interface Design
+                  </HoveredLink>
+                  <HoveredLink href="/seo">
+                    Search Engine Optimization
+                  </HoveredLink>
+                  <HoveredLink href="/branding">Branding</HoveredLink>
+                </div>
+              </MenuItem>
+            </div>
+          </Menu>
+        </div>
+      </div>
+    </>
   );
 }
--- components/ui/navbar-menu.tsx
+++ components/ui/navbar-menu.tsx
@@ -18,44 +18,48 @@ export const MenuItem = ({
   active,
   item,
   children,
+  Textcolor,
 }: {
   setActive: (item: string) => void;
   active: string | null;
+  Textcolor: boolean ;
   item: string;
   children?: React.ReactNode;
 }) => {
   return (
-    <div onMouseEnter={() => setActive(item)} className="relative ">
-      <motion.p
-        transition={{ duration: 0.3 }}
-        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
-      >
-        {item}
-      </motion.p>
-      {active !== null && (
-        <motion.div
-          initial={{ opacity: 0, scale: 0.85, y: 10 }}
-          animate={{ opacity: 1, scale: 1, y: 0 }}
-          transition={transition}
+    <div  className=" flex items-center justify-center text-sm  text-red md:w-[full]  lg:w-[full] trans ">
+      <div onMouseEnter={() => setActive(item)} className="relative ">
+        <motion.p
+          transition={{ duration: 0.3 }}
+          className={` px-[20px] py-[10px] w-full text-center cursor-pointer text-black hover:opacity-90 ${Textcolor ? "dark:text-black" : "dark:text-white"}`}
         >
-          {active === item && (
-            <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
-              <motion.div
-                transition={transition}
-                layoutId="active" // layoutId ensures smooth animation
-                className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
-              >
+          {item}
+        </motion.p>
+        {active !== null && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.85, y: 10 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            transition={transition}
+          >
+            {active === item && (
+              <div className="absolute left-1/2 top-[calc(100%_+_1.2rem)] -translate-x-1/2 pt-4">
                 <motion.div
-                  layout // layout ensures smooth animation
-                  className="w-max h-full p-4"
+                  transition={transition}
+                  layoutId="active" // layoutId ensures smooth animation
+                  className="overflow-hidden rounded-2xl border border-black/[0.2] bg-white shadow-xl backdrop-blur-sm dark:border-white/[0.2] dark:bg-black "
                 >
-                  {children}
+                  <motion.div
+                    layout // layout ensures smooth animation
+                    className="h-full w-max p-4"
+                  >
+                    {children}
+                  </motion.div>
                 </motion.div>
-              </motion.div>
-            </div>
-          )}
-        </motion.div>
-      )}
+              </div>
+            )}
+          </motion.div>
+        )}
+      </div>
     </div>
   );
 };
@@ -70,7 +74,7 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 "
+      className="text-nowrap relative flex justify-center rounded-full md:space-x-[0px]  lg:space-x-[20px] border border-transparent bg-white md:px-5 lg:px-20 py-4 shadow-input dark:border-white/[0.2] dark:bg-black  "
     >
       {children}
     </nav>
@@ -95,13 +99,13 @@ export const ProductItem = ({
         width={140}
         height={70}
         alt={title}
-        className="flex-shrink-0 rounded-md shadow-2xl"
+        className="shrink-0 rounded-md shadow-2xl"
       />
       <div>
-        <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
+        <h4 className="mb-1 text-xl font-bold text-black dark:text-white">
           {title}
         </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
+        <p className="max-w-40 text-sm text-neutral-700 dark:text-neutral-300">
           {description}
         </p>
       </div>
@@ -113,7 +117,7 @@ export const HoveredLink = ({ children, ...rest }: any) => {
   return (
     <Link
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
+      className="text-neutral-700 hover:text-black dark:text-neutral-200 "
     >
       {children}
     </Link>
