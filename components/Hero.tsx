7049fd58aa3b0c1aed73dd29d50a422a1c7cfa15
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-27 12:18:27 +1030

    // <span className="  animate__animated animate__zoomInDown flex flex-row"> //         <h1 //           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}} //           data-aos="fade-up" //           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl" //         > //           L //         </h1> //         <h1 //           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}} //           data-aos="fade-up" //           data-aos-offset="100" //           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl" //         > //           e //         </h1> //         <h1 //           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}} //           data-aos="fade-up" //           data-aos-offset="200" //           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl" //         > //           a //         </h1> //         <h1 //           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}} //           data-aos="fade-up" //           data-aos-offset="300" //           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl" //         > //           r //         </h1> //         <h1 //           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}} //           data-aos="fade-up" //           data-aos-offset="400" //           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl" //         > //           n //         </h1>
    //         </span>

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -56,7 +56,7 @@ const Hero = () => {
           Grow
         </h1>
       </div>
-      <BentoGrid className="max-w-4xl mx-auto">
+      {/* <BentoGrid className="max-w-4xl mx-auto">
         {items.map((item, i) => (
           <BentoGridItem
             key={i}
@@ -67,46 +67,97 @@ const Hero = () => {
             className={i === 3 || i === 6 ? "md:col-span-2" : ""}
           />
         ))}
-      </BentoGrid>
+      </BentoGrid> */}
     </div>
   );
 };
 
 export default Hero;
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-);
-const items = [
-  {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Pursuit of Knowledge",
-    description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton />,
-    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-  },
+// const Skeleton = () => (
+//   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+// );
+// const items = [
+//   {
+//     title: "The Dawn of Innovation",
+//     description: "Explore the birth of groundbreaking ideas and inventions.",
+//     header: <Skeleton />,
+//     icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+//   },
+//   {
+//     title: "The Digital Revolution",
+//     description: "Dive into the transformative power of technology.",
+//     header: <Skeleton />,
+//     icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+//   },
+//   {
+//     title: "The Art of Design",
+//     description: "Discover the beauty of thoughtful and functional design.",
+//     header: <Skeleton />,
+//     icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+//   },
+//   {
+//     title: "The Power of Communication",
+//     description:
+//       "Understand the impact of effective communication in our lives.",
+//     header: <Skeleton />,
+//     icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+//   },
+//   {
+//     title: "The Pursuit of Knowledge",
+//     description: "Join the quest for understanding and enlightenment.",
+//     header: <Skeleton />,
+//     icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+//   },
   
-];
+// ];
+
+
+
+
+
+
+
+
+// <span className="  animate__animated animate__zoomInDown flex flex-row">
+//         <h1
+//           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}}
+//           data-aos="fade-up"
+//           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl"
+//         >
+//           L
+//         </h1>
+//         <h1
+//           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}}
+//           data-aos="fade-up"
+//           data-aos-offset="100"
+//           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl"
+//         >
+//           e
+//         </h1>
+//         <h1
+//           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}}
+//           data-aos="fade-up"
+//           data-aos-offset="200"
+//           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl"
+//         >
+//           a
+//         </h1>
+//         <h1
+//           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}}
+//           data-aos="fade-up"
+//           data-aos-offset="300"
+//           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl"
+//         >
+//           r
+//         </h1>
+//         <h1
+//           //  style={{filter:"drop-shadow(0 0 5px #0752E9)"}}
+//           data-aos="fade-up"
+//           data-aos-offset="400"
+//           className="  drop-shadow-lg drop    bg-gradient-to-b from-[#0752E9] backdrop-blur-xl to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-9xl"
+//         >
+//           n
+//         </h1>
+
+
+//         </span>
